a4ddaaaaa85b756db7a21db927977bae839f1978
Vasiliy Kaygorodov <vkaygorodov@gmail.com>
2016-09-19 17:41:53 +0200

    Use built-in self.log instead of logging module; support HTTPBasicAuth; dont obtain an auth cookie on each request

--- jira.py
+++ jira.py
@@ -1,5 +1,4 @@
 from errbot import BotPlugin
-import logging
 import re
 import requests
 from requests_kerberos import HTTPKerberosAuth, DISABLED
@@ -29,6 +28,7 @@ class Jira(BotPlugin):
         received when calling the "!plugin config JIRA" command.
         """
 
+        self['COOKIE'] = None
         if configuration is not None and configuration != {}:
             config = dict(chain(CONFIG_TEMPLATE.items(),
                                 configuration.items()))
@@ -38,23 +38,35 @@ class Jira(BotPlugin):
 
 
     def get_cookie(self):
-        r = requests.get(self.config['URL']+'/step-auth-gss',
-                         auth=HTTPKerberosAuth(mutual_authentication=DISABLED),
-                         verify=False)
-        return r.cookies['JSESSIONID']
+        self.log.debug("[JIRA] Requesting JIRA cookie")
+        if not self['COOKIE']:
+            r = requests.get(self.config['URL']+'/step-auth-gss',
+                             auth=HTTPKerberosAuth(mutual_authentication=DISABLED),
+                             verify=False)
+            self['COOKIE'] = r.cookies['JSESSIONID']
+        return self['COOKIE']
 
     def get_issue(self, issue_id):
         """Retrieves issue JSON from JIRA"""
-        try:
-            cookie = self.get_cookie()
-            logging.info("[JIRA] got cookie %s" % cookie)
-        except:
-            cookie = ''
+        if self.config['KERBEROS']:
+            auth = (self.config('USERNAME'), self.config('PASSWORD'))
+            headers = {}
+        else:
+            try:
+                cookie = self.get_cookie()
+                self.log.debug("[JIRA] got cookie %s" % cookie)
+            except Exception, ex:
+                self.log.error("[JIRA] did not get a cookie: %s" % ex.mess)
+                cookie = ''
+            auth = None
+            headers = {"Cookie": "JSESSIONID=%s" % cookie}
+
         response = requests.get(
             self.config['URL']+'/rest/api/latest/issue/'+issue_id+'.json',
-            headers={"Cookie": "JSESSIONID=%s" % cookie},
+            auth=auth,
+            headers=headers,
             verify=self.config('SSL_VERIFY'))
-        logging.info("[JIRA] got response %s" % response.status_code)
+        self.log.info("[JIRA] got response %s" % response.status_code)
         return response
 
     def get_response_text(self, mess):
@@ -69,19 +81,19 @@ class Jira(BotPlugin):
                 # set() gives us uniques, but does not preserve order.
                 for match in set(matches):
                     issue_id = match
-                    logging.info("[JIRA] matched issue_id: %s" % issue_id)
+                    self.log.info("[JIRA] matched issue_id: %s" % issue_id)
                     issue_response = self.get_issue(issue_id)
                     if issue_response.status_code in (200,):
-                        logging.info("[JIRA] retrieved issue data: %s" % issue_response)
+                        self.log.info("[JIRA] retrieved issue data: %s" % issue_response)
                         issue_summary = issue_response.json()['fields']['summary']
                         return "%s/browse/%s - %s" % (self.config['URL'], issue_id, issue_summary)
                     elif issue_response.status_code in (401,):
-                        logging.error("[JIRA] Access Denied")
+                        self.log.error("[JIRA] Access Denied")
                     elif issue_response.status_code in (404,):
                         return "Issue not found"
                     else:
-                        logging.error("[JIRA] encountered unknown response status code: %s" % issue_response.status_code)
-                        logging.error("[JIRA] response body: %s" % issue_response.json())
+                        self.log.error("[JIRA] encountered unknown response status code: %s" % issue_response.status_code)
+                        self.log.error("[JIRA] response body: %s" % issue_response.json())
 
     def callback_message(self, mess):
         """A callback which responds to mention of JIRA issues"""
